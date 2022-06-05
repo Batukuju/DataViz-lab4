8bd3d2cbad4aa876d2819da79abd4bd971ae292e
Batukuju <50993098+Batukuju@users.noreply.github.com>
2022-06-06 00:22:54 +0200

    Last push...

--- web_app_v2/app.R
+++ web_app_v2/app.R
@@ -1,11 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 library(ggstream)
 library(ggplot2)
 library(dplyr)
@@ -54,89 +46,98 @@ raw_data <- data.frame(read.csv("songs_normalize.csv")) %>%
   select(-col, -genre_temp)
 
 data <- raw_data %>%
-  separate(col = genre, into = paste("col", 1:4), sep = ", ", ,fill = "right", extra = "drop") %>%
-  gather(`genre`, key = "col", 18:21, na.rm = TRUE) %>%
-  distinct(.keep_all = TRUE) %>%
-  select(-col) %>%
-  mutate(song = paste(song, artist, year, popularity, sep = "__"))
+  mutate(song_key = paste(song, artist, year, popularity, sep = "__"))
 
 #some additional helpful variables
 summaryPopularity <- summary(raw_data$popularity)
 summaryDanceability <- summary(raw_data$danceability)
 summaryDuration <- summary(raw_data$`duration[min]`)
 genres = unique(raw_data$genre)
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
   #different themes
-  #theme = shinytheme("sandstone"),
+  theme = shinytheme("sandstone"),
   
-  # Application title
-  titlePanel("Top 2000 Spotify songs"),
+  #application title
+  titlePanel("Analyzer of the best songs from Spotify"),
   
   
-  navbarPage("Menu",
+  navbarPage("",
                
     tabPanel("Summary",
-      fluidRow(
-        #Main area for "future plots"
-        column(12,
-               "two plots",
-               plotlyOutput("mainPlot"),
-               #tableOutput("table")
-               plotlyOutput("summaryPlot2")
-               
+        fluidRow(
+          h2("Comparison of the entire dataset"),
+          h5("Compare the volume of different genres in the dataset and see how the volume changes in the scope of past years"),
+          #Main area for "future plots"
+          column(8,
+                 plotlyOutput("mainPlot"),
+                 #tableOutput("table")
+                 
+          ),
+          column(4,
+                 plotlyOutput("summaryPlot2")
+          )
         ),
-      )
+        h5("Songs matching your preference (see \"Search\" tab)"),
+        tableOutput("table2")
     ),
     tabPanel("Search",
       fluidRow(
         h2("Choose your preferences and compare your result"),
-        column(5,
-               #popularity slider
-               sidebarPanel(width = 12,
-                            sliderInput("popularitySlider",
-                                        "Choose popularity:",
-                                        min = 0,
-                                        max = 100,
-                                        value = c(50, 75)),
-                            #duration slider
-                            sliderInput("durationSlider",
-                                        "Choose duration [minutes]:",
-                                        min = 0,
-                                        max = summaryDuration[[6]],
-                                        value = c(2, 5)),
-                            #danceability slider
-                            sliderInput("danceabilitySlider",
-                                        "Choose danceability:",
-                                        min = 0,
-                                        max = 100,
-                                        value = c(25, 50)),
-                            #selection of genres
-                            selectInput("genreChoice",
-                                        "Choose genres:",
-                                        choices = genres,
-                                        multiple = TRUE,
-                                        selected = c("pop", "rock")
-                            )
+        h5("Choose values of attributes you want to display found songs. You can also compare characteristics of different genres"),
+        column(4,
+                #number of songs to display
+                numericInput("numberOfSongsToDisplay",
+                            "How many songs to display below:",
+                            value = 20,
+                            min = 0,
+                            max = 4000
+                            ),
+                #popularity slider
+                sliderInput("popularitySlider",
+                            "Choose popularity:",
+                            min = 0,
+                            max = 100,
+                            value = c(50, 75)),
+                #duration slider
+                sliderInput("durationSlider",
+                            "Choose duration [minutes]:",
+                            min = 0,
+                            max = summaryDuration[[6]],
+                            value = c(2, 5)),
+                #danceability slider
+                sliderInput("danceabilitySlider",
+                            "Choose danceability:",
+                            min = 0,
+                            max = 100,
+                            value = c(25, 50)),
+                #selection of genres
+                selectInput("genreChoice",
+                            "Choose genres:",
+                            choices = genres,
+                            multiple = TRUE,
+                            selected = c("pop", "rock")
+                
                ),
                #"crazy" valueBox
                valueBox(value = "songsNumber",
-                        subtitle = "Number of songs",
+                        subtitle = "Number of songs matching preference",
                         icon = "hashtag",
                         color = "bg-info")
                ),
-        column(7,
+        column(8,
                #plot parcord chart
                plotlyOutput("summaryPlot"),
                #gauge
                gaugeOutput("gauge")
                )
-      )
-      
+      ),
+      h5("Songs matching your preference"),
+      tableOutput("table")
     ) 
   ) 
 )
-# Define server logic required to draw a histogram
+#define server logic required to draw a histogram
 server <- function(input, output) {
     #data from "Search" section, reevaluated for later use
     reactive_data <- reactive({
@@ -152,34 +153,52 @@ server <- function(input, output) {
         arrange(-popularity)
     })
     
-    ###MAIN PLOTS <- I couldn't make them interactive in no way. :/
-    
-    #I have no idea, why this doesn't work :/ 
-    
-    output$mainPlot <- renderPlotly({
-      bubble_attributes<-c("popularity", "genre", "song")
+    reactive_mainPlot <- reactive({
+      bubble_attributes<-c("popularity", "genre", "song_key")
       data_bubble<-data %>%
         select(bubble_attributes)
       edges_genre<-data_bubble %>% transmute( from = 'origin', to = paste('origin',genre, sep="."))
-      edges_song<-data_bubble%>% transmute( from = paste('origin',genre, sep="."), to = paste('origin',genre, song, sep = "."))
+      edges_song<-data_bubble%>% transmute( from = paste('origin',genre, sep="."), to = paste('origin',genre, song_key, sep = "."))
       edges<- unique(bind_rows(edges_song, edges_genre))
       edges<-rbind(edges,c('',"origin"))
-      
-      plot_ly(  type="treemap",
-                labels=as.vector(edges$to),
-                parents=as.vector(edges$from))
+      edges
     })
     
-    #parcoord plot summary
-    output$summaryPlot <- renderPlotly({
-      
+    reactive_parcoordPlot <- reactive({
       selected_attributes<-c("duration[min]", "danceability", "energy", "loudness", "speechiness", "genre")
       selected_data<-data %>% 
         dplyr::select(selected_attributes)%>%
         filter(genre %in% input$genreChoice) %>%
         group_by(genre)%>%
         summarise_all(funs(mean))
-      p <- ggparcoord(selected_data,columns = 2:length(selected_attributes), groupColumn = 1)+
+      selected_data
+    })
+    
+    reactive_barPlot <- reactive({
+      geom_attributes<-c("year","popularity", "genre")
+      data_geomstream<-data %>% 
+        dplyr::select(geom_attributes) %>%
+        filter(year>=2000)%>%
+        group_by(year,genre) %>%
+        count() %>%
+        mutate(genre = other(n,genre)) %>%
+        rename(`number of songs` = n)
+      data_geomstream$genre<-reorder(data_geomstream$genre, data_geomstream$`number of songs`)
+      data_geomstream
+    })
+
+    output$mainPlot <- renderPlotly({
+      plot_ly(  type="treemap",
+                labels=as.vector(reactive_mainPlot()$to),
+                parents=as.vector(reactive_mainPlot()$from)) %>%
+        layout(title = 'Volume of different genres', titlefont=list(size=18))
+    })
+    
+    #parcoord plot summary
+    output$summaryPlot <- renderPlotly({
+      
+      selected_attributes<-c("duration[min]", "danceability", "energy", "loudness", "speechiness", "genre")
+      p <- ggparcoord(reactive_parcoordPlot(),columns = 2:length(selected_attributes), groupColumn = 1)+
         theme_minimal()+
         theme(
           plot.title = element_text(size=18),
@@ -197,27 +216,17 @@ server <- function(input, output) {
     }
     #barchart summary
     output$summaryPlot2 <- renderPlotly({
-
-      geom_attributes<-c("year","popularity", "genre")
-      
-      data_geomstream<-data %>% 
-        dplyr::select(geom_attributes) %>%
-        filter(year>=2000)%>%
-        group_by(year,genre) %>%
-        count() %>%
-        mutate(genre = other(n,genre)) %>%
-        rename(`number of songs` = n)
-      
-      data_geomstream$genre<-reorder(data_geomstream$genre, data_geomstream$`number of songs`)
-      
-      p <- ggplot(data_geomstream, aes(fill=genre, y=`number of songs`, x=year)) + 
-        geom_bar(position='stack', stat='identity')
+      p <- ggplot(reactive_barPlot(), aes(fill=genre, y=`number of songs`, x=year)) + 
+        geom_bar(position='stack', stat='identity') +
+        theme(
+          plot.title = element_text(size=18)
+        ) + labs(title = "Barplot of genres in respect of the time")
       ggplotly(p)
-      
     })
     
     #renders a table
-    output$table <- renderTable(reactive_data())
+    output$table <- renderTable(head(reactive_data(), n = input$numberOfSongsToDisplay), striped = TRUE,  hover = TRUE, rownames = TRUE)
+    output$table2 <- renderTable(head(reactive_data(), n = input$numberOfSongsToDisplay), striped = TRUE,  hover = TRUE, rownames = TRUE)
     
     #renders number of songs
     output$songsNumber<- renderText({ 
@@ -230,12 +239,10 @@ server <- function(input, output) {
       gauge(round(ave(x)[1], 2),
             label = "Average popularity",
             min = 0, 
-            max = 100#, 
-            #sectors = gaugeSectors(success = c(0.5, 1), 
-            #                       warning = c(0., 0.5),
+            max = 100
       )
     })
 }
 
-# Run the application 
+#run the application 
 shinyApp(ui = ui, server = server)
